7890170b99a6be79eebd61ca10b0448b330a2b77
Nikita <nikita.kosilo@gmail.com>
2016-07-27 18:57:54 +0300

    Update index.js
    Change video source on component update

--- src/index.js
+++ src/index.js
@@ -1,39 +1,54 @@
+'use strict';
+
 var assign = require('object-assign');
 var cx = require('classnames');
 var blacklist = require('blacklist');
 var React = require('react');
 
 module.exports = React.createClass({
-  displayName: 'VideoJS',
+    displayName : 'VideoJS',
 
-  componentDidMount() {
-    var self = this;
-    var player = videojs(this.refs.video, this.props.options).ready(function() {
-      self.player = this
-      self.player.on('play', self.handlePlay);
-    });
-    if(this.props.onPlayerInit) this.props.onPlayerInit(player);
-  },
+    componentDidMount () {
+        var self = this;
+        var player = videojs(this.refs.video, this.props.options)
+            .ready(function () {
+                self.player = this;
+                self.player.on('play', self.handlePlay);
+                self.player.autoplay(true);
+            });
+        if (this.props.onPlayerInit) this.props.onPlayerInit(player);
+    },
 
-  handlePlay: function() {
-    if(this.props.onPlay) this.props.onPlay(this.player);
-  },
+    componentDidUpdate (prevProps) {
+        if (this.props.src !== prevProps.src) {
+            if (this.hasOwnProperty('player') && this.player) {
+                this.player.src({
+                    type : this.props.type,
+                    src  : this.props.src
+                });
+            }
+        }
+    },
 
-  render() {
-    var props = blacklist(this.props, 'children', 'className', 'src', 'type', 'onPlay');
-    props.className = cx(this.props.className, 'videojs', 'video-js vjs-default-skin');
+    handlePlay () {
+        if (this.props.onPlay) this.props.onPlay(this.player);
+    },
 
-    assign(props, {
-      ref: 'video',
-      controls: true
-    });
+    render () {
+        var props = blacklist(this.props, 'children', 'className', 'src', 'type', 'onPlay');
+        props.className = cx(this.props.className, 'videojs', 'video-js vjs-default-skin');
 
-    return (
-      <div>
-        <video {... props}>
-          <source src={this.props.src} type={this.props.type}/>
-        </video>
-      </div>
-    )
-  }
+        assign(props, {
+            ref      : 'video',
+            controls : true
+        });
+
+        return (
+            <div>
+                <video {... props}>
+                    <source src={this.props.src} type={this.props.type}/>
+                </video>
+            </div>
+        )
+    }
 });
